a21a3659e0c4722123798d7b5f67e03394422393
mariamarcub <mmarcub912@iesmartinezm.es>
2023-10-25 10:45:06 +0200

    nueva mod

--- tablero/urls.py
+++ tablero/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('crea_tablero/', views.crea_tablero, name='crea_tablero'),
 ]
--- tablero/views.py
+++ tablero/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'tablero/index.html', {})
+
+
+
+def crea_tablero(request):
+    return render(request, 'tablero/crea_tablero.html', {})
